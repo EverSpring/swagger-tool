507cec93c91c3c03b59d54a0301c2c640adaf02d
changchun.xue <xuechangchun007@126.com>
2021-01-13 20:28:30 +0800

    添加alt+insert快捷键

--- src/com/everspring/action/SetAction.java
+++ src/com/everspring/action/SetAction.java
@@ -3,6 +3,10 @@ package com.everspring.action;
 import com.everspring.view.SetDialogForm;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Description： 生成set的实现
@@ -16,5 +20,17 @@ public class SetAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
         SetDialogForm setDialogForm = new SetDialogForm(e);
         setDialogForm.show();
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        super.update(e);
+        PsiFile psiFile = e.getRequiredData(CommonDataKeys.PSI_FILE);
+        FileType fileType = psiFile.getViewProvider().getVirtualFile().getFileType();
+        if (!"java".equals(fileType.getDefaultExtension())) {
+            e.getPresentation().setEnabledAndVisible(false);
+        } else {
+            e.getPresentation().setEnabledAndVisible(true);
+        }
     }
 }
